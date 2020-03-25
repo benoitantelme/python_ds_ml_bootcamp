6a43c4ad519497c0d966738b0fb95d244148d820
benoitantelme <benoitantelme@gmail.com>
2020-03-25 10:51:11 +0000

    Converted jupyter notebooks to python scripts

---
+++ 00-Welcome to the Course.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ___
+# 
+# <a href='http://www.pieriandata.com'> <img src='Pierian_Data_Logo.png' /></a>
+# ___
+# # Welcome to the Course 
+# 
+# This is the repository for the course: **Python for Data Science and Machine Learning Bootcamp**
+# 
+# Thank you so much for enrolling in the course!
+# 
+# ## Overview of this Repository
+# 
+# This repository contains everything you need for the course, it has all the notebooks, links
