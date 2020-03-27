5a27dcee48a0acf47b8a7f10b98fd976a36c6f69
benoitantelme <benoitantelme@gmail.com>
2020-03-27 22:09:25 +0000

    pandas second exercises

--- 04-Pandas-Exercises/03-Ecommerce Purchases Exercise .py
+++ 04-Pandas-Exercises/03-Ecommerce Purchases Exercise .py
@@ -7,7 +7,9 @@
 # ___
 # # Ecommerce Purchases Exercise
 # 
-# In this Exercise you will be given some Fake Data about some purchases done through Amazon! Just go ahead and follow the directions and try your best to answer the questions and complete the tasks. Feel free to reference the solutions. Most of the tasks can be solved in different ways. For the most part, the questions get progressively harder.
+# In this Exercise you will be given some Fake Data about some purchases done through Amazon! Just go ahead and follow
+# the directions and try your best to answer the questions and complete the tasks. Feel free to reference the solutions.
+# Most of the tasks can be solved in different ways. For the most part, the questions get progressively harder.
 # 
 # Please excuse anything that doesn't make "Real-World" sense in the dataframe, all the data is fake and made-up.
 # 
@@ -17,127 +19,90 @@
 
 # In[84]:
 
-
-
-
+import pandas as pd
 
 # In[86]:
 
-
-
-
+ecom = pd.read_csv("Ecommerce Purchases")
 
 # **Check the head of the DataFrame.**
 
 # In[87]:
 
-
-
-
+print(ecom.head())
 
 # ** How many rows and columns are there? **
 
 # In[88]:
 
 
-
-
+print(ecom.info())
 
 # ** What is the average Purchase Price? **
 
 # In[90]:
-
-
-
+print(ecom['Purchase Price'].mean())
 
 
 # ** What were the highest and lowest purchase prices? **
 
 # In[92]:
-
-
-
-
+print(ecom['Purchase Price'].max())
 
 # In[93]:
-
-
-
-
+print(ecom['Purchase Price'].min())
 
 # ** How many people have English 'en' as their Language of choice on the website? **
 
 # In[94]:
-
-
-
-
+print(len(ecom[ecom['Language'] == 'en']))
+print(ecom[ecom['Language'] == 'en'].count())
 
 # ** How many people have the job title of "Lawyer" ? **
 # 
 
 # In[95]:
-
-
-
-
+print(len(ecom[ecom['Job'] == "Lawyer"]))
 
 # ** How many people made the purchase during the AM and how many people made the purchase during PM ? **
 # 
-# **(Hint: Check out [value_counts()](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.value_counts.html) ) **
+# **(Hint: Check out [value_counts()]
+# (http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.value_counts.html) ) **
 
 # In[96]:
-
-
-
-
+print(ecom['AM or PM'].value_counts())
 
 # ** What are the 5 most common Job Titles? **
 
 # In[97]:
-
-
-
-
+print(ecom['Job'].value_counts().head(5))
 
 # ** Someone made a purchase that came from Lot: "90 WT" , what was the Purchase Price for this transaction? **
 
 # In[99]:
-
-
-
+print(ecom[ecom['Lot'] == '90 WT']['Purchase Price'])
 
 
 # ** What is the email of the person with the following Credit Card Number: 4926535242672853 **
 
 # In[100]:
-
-
-
+print(ecom[ecom['Credit Card'] == 4926535242672853]['Email'])
 
 
 # ** How many people have American Express as their Credit Card Provider *and* made a purchase above $95 ?**
 
 # In[101]:
-
-
-
+print(len(ecom[(ecom['CC Provider'] == 'American Express') & (ecom['Purchase Price'] > 95.00)]))
 
 
 # ** Hard: How many people have a credit card that expires in 2025? **
 
 # In[102]:
 
-
-
-
+print(sum(ecom['CC Exp Date'].apply(lambda x: x.split('/')[1] == '25')))
 
 # ** Hard: What are the top 5 most popular email providers/hosts (e.g. gmail.com, yahoo.com, etc...) **
 
 # In[56]:
-
-
-
-
-
+print(ecom['Email'].apply(lambda x: x.split('@')[1]).value_counts().head(5))
 # # Great Job!
