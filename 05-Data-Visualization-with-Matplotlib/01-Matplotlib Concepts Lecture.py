4234da978bc4e8b2d988299c63d580fe0ea674e2
benoitantelme <benoitantelme@gmail.com>
2020-03-28 14:55:28 +0000

    matplotlib class

--- 05-Data-Visualization-with-Matplotlib/01-Matplotlib Concepts Lecture.py
+++ 05-Data-Visualization-with-Matplotlib/01-Matplotlib Concepts Lecture.py
@@ -9,7 +9,9 @@
 
 # ## Introduction
 
-# Matplotlib is the "grandfather" library of data visualization with Python. It was created by John Hunter. He created it to try to replicate MatLab's (another programming language) plotting capabilities in Python. So if you happen to be familiar with matlab, matplotlib will feel natural to you.
+# Matplotlib is the "grandfather" library of data visualization with Python. It was created by John Hunter. He created
+# it to try to replicate MatLab's (another programming language) plotting capabilities in Python. So if you happen to be
+# familiar with matlab, matplotlib will feel natural to you.
 # 
 # It is an excellent 2D and 3D graphics library for generating scientific figures. 
 # 
@@ -21,7 +23,8 @@
 # * High-quality output in many formats
 # * Very customizable in general
 # 
-# Matplotlib allows you to create reproducible figures programmatically. Let's learn how to use it! Before continuing this lecture, I encourage you just to explore the official Matplotlib web page: http://matplotlib.org/
+# Matplotlib allows you to create reproducible figures programmatically. Let's learn how to use it! Before continuing
+# this lecture, I encourage you just to explore the official Matplotlib web page: http://matplotlib.org/
 # 
 # ## Installation 
 # 
@@ -40,16 +43,16 @@
 
 import matplotlib.pyplot as plt
 
-
 # You'll also need to use this line to see plots in the notebook:
 
 # In[2]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# That line is only for jupyter notebooks, if you are using another editor, you'll use: **plt.show()** at the end of all your plotting commands to have the figure pop up in another window.
+# That line is only for jupyter notebooks, if you are using another editor, you'll use: **plt.show()** at the end of
+# all your plotting commands to have the figure pop up in another window.
 
 # # Basic Example
 # 
@@ -57,7 +60,8 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # ### Example
 # 
-# Let's walk through a very simple example using two numpy arrays. You can also use lists, but most likely you'll be passing numpy arrays or pandas columns (which essentially also behave like arrays).
+# Let's walk through a very simple example using two numpy arrays. You can also use lists, but most likely you'll be
+# passing numpy arrays or pandas columns (which essentially also behave like arrays).
 # 
 # ** The data we want to plot:**
 
@@ -65,35 +69,35 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 import numpy as np
+
 x = np.linspace(0, 5, 11)
 y = x ** 2
-
+# plt.plot(x, y)
+# plt.show()
 
 # In[8]:
 
 
-x
-
+# x
 
 # In[10]:
 
 
-y
-
+# y
 
 # ## Basic Matplotlib Commands
 # 
-# We can create a very simple line plot using the following ( I encourage you to pause and use Shift+Tab along the way to check out the document strings for the functions we are using).
+# We can create a very simple line plot using the following ( I encourage you to pause and use Shift+Tab along the way
+# to check out the document strings for the functions we are using).
 
 # In[13]:
 
 
-plt.plot(x, y, 'r') # 'r' is the color red
+plt.plot(x, y, 'r')  # 'r' is the color red
 plt.xlabel('X Axis Title Here')
 plt.ylabel('Y Axis Title Here')
 plt.title('String Title Here')
-plt.show()
-
+# plt.show()
 
 # ## Creating Multiplots on Same Canvas
 
@@ -101,19 +105,22 @@ plt.show()
 
 
 # plt.subplot(nrows, ncols, plot_number)
-plt.subplot(1,2,1)
-plt.plot(x, y, 'r--') # More on color options later
-plt.subplot(1,2,2)
-plt.plot(y, x, 'g*-');
-
+plt.subplot(1, 2, 1)
+plt.plot(x, y, 'r--')  # More on color options later
+plt.subplot(1, 2, 2)
+plt.plot(y, x, 'g*-')
+# plt.show()
 
 # ___
 # # Matplotlib Object Oriented Method
-# Now that we've seen the basics, let's break it all down with a more formal introduction of Matplotlib's Object Oriented API. This means we will instantiate figure objects and then call methods or attributes from that object.
+# Now that we've seen the basics, let's break it all down with a more formal introduction of Matplotlib's Object
+# Oriented API. This means we will instantiate figure objects and then call methods or attributes from that object.
 
 # ## Introduction to the Object Oriented Method
 
-# The main idea in using the more formal Object Oriented method is to create figure objects and then just call methods or attributes off of that object. This approach is nicer when dealing with a canvas that has multiple plots on it. 
+# The main idea in using the more formal Object Oriented method is to create figure objects and then just call
+# methods or attributes off of that object. This approach is nicer when dealing with a canvas that has multiple plots
+# on it.
 # 
 # To begin we create a figure instance. Then we can add axes to that figure:
 
@@ -124,16 +131,16 @@ plt.plot(y, x, 'g*-');
 fig = plt.figure()
 
 # Add set of axes to figure
-axes = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # left, bottom, width, height (range 0 to 1)
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])  # left, bottom, width, height (range 0 to 1)
 
 # Plot on that set of axes
 axes.plot(x, y, 'b')
-axes.set_xlabel('Set X Label') # Notice the use of set_ to begin methods
+axes.set_xlabel('Set X Label')  # Notice the use of set_ to begin methods
 axes.set_ylabel('Set y Label')
 axes.set_title('Set Title')
 
-
-# Code is a little more complicated, but the advantage is that we now have full control of where the plot axes are placed, and we can easily add more than one axis to the figure:
+# Code is a little more complicated, but the advantage is that we now have full control of where the plot axes are
+# placed, and we can easily add more than one axis to the figure:
 
 # In[16]:
 
@@ -141,20 +148,20 @@ axes.set_title('Set Title')
 # Creates blank canvas
 fig = plt.figure()
 
-axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # main axes
-axes2 = fig.add_axes([0.2, 0.5, 0.4, 0.3]) # inset axes
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])  # main axes
+axes2 = fig.add_axes([0.2, 0.5, 0.4, 0.3])  # inset axes
 
 # Larger Figure Axes 1
 axes1.plot(x, y, 'b')
-axes1.set_xlabel('X_label_axes2')
-axes1.set_ylabel('Y_label_axes2')
-axes1.set_title('Axes 2 Title')
+axes1.set_xlabel('X_label_axes1')
+axes1.set_ylabel('Y_label_axes1')
+axes1.set_title('Axes 1 Title')
 
 # Insert Figure Axes 2
 axes2.plot(y, x, 'r')
 axes2.set_xlabel('X_label_axes2')
 axes2.set_ylabel('Y_label_axes2')
-axes2.set_title('Axes 2 Title');
+axes2.set_title('Axes 2 Title')
 
 
 # ## subplots()
@@ -173,8 +180,7 @@ fig, axes = plt.subplots()
 axes.plot(x, y, 'r')
 axes.set_xlabel('x')
 axes.set_ylabel('y')
-axes.set_title('title');
-
+axes.set_title('title')
 
 # Then you can specify the number of rows and columns when creating the subplots() object:
 
@@ -184,13 +190,11 @@ axes.set_title('title');
 # Empty canvas of 1 by 2 subplots
 fig, axes = plt.subplots(nrows=1, ncols=2)
 
-
 # In[25]:
 
 
 # Axes is an array of axes to plot on
 axes
-
 
 # We can iterate through this array:
 
@@ -206,27 +210,38 @@ for ax in axes:
 # Display the figure object    
 fig
 
-
-# A common issue with matplolib is overlapping subplots or figures. We ca use **fig.tight_layout()** or **plt.tight_layout()** method, which automatically adjusts the positions of the axes on the figure canvas so that there is no overlapping content:
+# A common issue with matplolib is overlapping subplots or figures. We ca use **fig.tight_layout()** or
+# **plt.tight_layout()** method, which automatically adjusts the positions of the axes on the figure canvas so that
+# there is no overlapping content:
 
 # In[32]:
 
-
 fig, axes = plt.subplots(nrows=1, ncols=2)
 
-for ax in axes:
-    ax.plot(x, y, 'g')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title('title')
+# for ax in axes:
+#     ax.plot(x, y, 'g')
+#     ax.set_xlabel('x')
+#     ax.set_ylabel('y')
+#     ax.set_title('title')
 
-fig    
+axes[0].plot(x, y, 'g')
+axes[0].set_xlabel('x')
+axes[0].set_ylabel('y')
+axes[0].set_title('plot 1')
+
+axes[1].plot(x, y, 'r')
+axes[1].set_xlabel('x')
+axes[1].set_ylabel('y')
+axes[1].set_title('plot 2')
+
+
+fig
 plt.tight_layout()
-
 
 # ### Figure size, aspect ratio and DPI
 
-# Matplotlib allows the aspect ratio, DPI and figure size to be specified when the Figure object is created. You can use the `figsize` and `dpi` keyword arguments. 
+# Matplotlib allows the aspect ratio, DPI and figure size to be specified when the Figure object is created. You can
+# use the `figsize` and `dpi` keyword arguments.
 # * `figsize` is a tuple of the width and height of the figure in inches
 # * `dpi` is the dots-per-inch (pixel per inch). 
 # 
@@ -235,21 +250,19 @@ plt.tight_layout()
 # In[33]:
 
 
-fig = plt.figure(figsize=(8,4), dpi=100)
-
+fig = plt.figure(figsize=(8, 4), dpi=100)
 
 # The same arguments can also be passed to layout managers, such as the `subplots` function:
 
 # In[34]:
 
 
-fig, axes = plt.subplots(figsize=(12,3))
+fig, axes = plt.subplots(figsize=(12, 3))
 
 axes.plot(x, y, 'r')
 axes.set_xlabel('x')
 axes.set_ylabel('y')
-axes.set_title('title');
-
+axes.set_title('title')
 
 # ## Saving figures
 # Matplotlib can generate high-quality output in a number formats, including PNG, JPG, EPS, SVG, PGF and PDF. 
@@ -257,10 +270,9 @@ axes.set_title('title');
 # To save a figure to a file we can use the `savefig` method in the `Figure` class:
 
 # In[68]:
-
+# plt.show()
 
 fig.savefig("filename.png")
-
 
 # Here we can also optionally specify the DPI and choose between different output formats:
 
@@ -269,21 +281,21 @@ fig.savefig("filename.png")
 
 fig.savefig("filename.png", dpi=200)
 
-
 # ____
 # ## Legends, labels and titles
 
-# Now that we have covered the basics of how to create a figure canvas and add axes instances to the canvas, let's look at how decorate a figure with titles, axis labels, and legends.
+# Now that we have covered the basics of how to create a figure canvas and add axes instances to the canvas,
+# let's look at how decorate a figure with titles, axis labels, and legends.
 
 # **Figure titles**
 # 
-# A title can be added to each axis instance in a figure. To set the title, use the `set_title` method in the axes instance:
+# A title can be added to each axis instance in a figure. To set the title, use the `set_title` method in the axes
+# instance:
 
 # In[41]:
 
 
 ax.set_title("title");
-
 
 # **Axis labels**
 # 
@@ -293,45 +305,46 @@ ax.set_title("title");
 
 
 ax.set_xlabel("x")
-ax.set_ylabel("y");
-
+ax.set_ylabel("y")
 
 # ### Legends
 
-# You can use the **label="label text"** keyword argument when plots or other objects are added to the figure, and then using the **legend** method without arguments to add the legend to the figure: 
+# You can use the **label="label text"** keyword argument when plots or other objects are added to the figure,
+# and then using the **legend** method without arguments to add the legend to the figure:
 
 # In[48]:
 
 
 fig = plt.figure()
 
-ax = fig.add_axes([0,0,1,1])
+ax = fig.add_axes([0, 0, 1, 1])
 
-ax.plot(x, x**2, label="x**2")
-ax.plot(x, x**3, label="x**3")
+ax.plot(x, x ** 2, label="x**2")
+ax.plot(x, x ** 3, label="x**3")
 ax.legend()
-
 
 # Notice how are legend overlaps some of the actual plot!
 # 
-# The **legend** function takes an optional keyword argument **loc** that can be used to specify where in the figure the legend is to be drawn. The allowed values of **loc** are numerical codes for the various places the legend can be drawn. See the [documentation page](http://matplotlib.org/users/legend_guide.html#legend-location) for details. Some of the most common **loc** values are:
+# The **legend** function takes an optional keyword argument **loc** that can be used to specify where in the figure
+# the legend is to be drawn. The allowed values of **loc** are numerical codes for the various places the legend can
+# be drawn. See the [documentation page](http://matplotlib.org/users/legend_guide.html#legend-location) for details.
+# Some of the most common **loc** values are:
 
 # In[52]:
 
 
 # Lots of options....
 
-ax.legend(loc=1) # upper right corner
-ax.legend(loc=2) # upper left corner
-ax.legend(loc=3) # lower left corner
-ax.legend(loc=4) # lower right corner
+ax.legend(loc=1)  # upper right corner
+ax.legend(loc=2)  # upper left corner
+ax.legend(loc=3)  # lower left corner
+ax.legend(loc=4)  # lower right corner
 
 # .. many more options are available
 
 # Most common to choose
-ax.legend(loc=0) # let matplotlib decide the optimal location
+ax.legend(loc=0)  # let matplotlib decide the optimal location
 fig
-
 
 # ## Setting colors, linewidths, linetypes
 # 
@@ -341,67 +354,68 @@ fig
 
 # ### Colors with MatLab like syntax
 
-# With matplotlib, we can define the colors of lines and other graphical elements in a number of ways. First of all, we can use the MATLAB-like syntax where `'b'` means blue, `'g'` means green, etc. The MATLAB API for selecting line styles are also supported: where, for example, 'b.-' means a blue line with dots:
+# With matplotlib, we can define the colors of lines and other graphical elements in a number of ways. First of all, we
+# can use the MATLAB-like syntax where `'b'` means blue, `'g'` means green, etc. The MATLAB API for selecting line
+# styles are also supported: where, for example, 'b.-' means a blue line with dots:
 
 # In[54]:
 
 
 # MATLAB style line color and style 
 fig, ax = plt.subplots()
-ax.plot(x, x**2, 'b.-') # blue line with dots
-ax.plot(x, x**3, 'g--') # green dashed line
-
+ax.plot(x, x ** 2, 'b.-')  # blue line with dots
+ax.plot(x, x ** 3, 'g--')  # green dashed line
 
 # ### Colors with the color= parameter
 
-# We can also define colors by their names or RGB hex codes and optionally provide an alpha value using the `color` and `alpha` keyword arguments. Alpha indicates opacity.
+# We can also define colors by their names or RGB hex codes and optionally provide an alpha value using the `color`
+# and `alpha` keyword arguments. Alpha indicates opacity.
 
 # In[56]:
 
 
 fig, ax = plt.subplots()
 
-ax.plot(x, x+1, color="blue", alpha=0.5) # half-transparant
-ax.plot(x, x+2, color="#8B008B")        # RGB hex code
-ax.plot(x, x+3, color="#FF8C00")        # RGB hex code 
-
+ax.plot(x, x + 1, color="blue", alpha=0.5)  # half-transparant
+ax.plot(x, x + 2, color="#8B008B")  # RGB hex code
+ax.plot(x, x + 3, color="#FF8C00")  # RGB hex code
 
 # ### Line and marker styles
 
-# To change the line width, we can use the `linewidth` or `lw` keyword argument. The line style can be selected using the `linestyle` or `ls` keyword arguments:
+# To change the line width, we can use the `linewidth` or `lw` keyword argument. The line style can be selected using
+# the `linestyle` or `ls` keyword arguments:
 
 # In[57]:
 
 
-fig, ax = plt.subplots(figsize=(12,6))
+fig, ax = plt.subplots(figsize=(12, 6))
 
-ax.plot(x, x+1, color="red", linewidth=0.25)
-ax.plot(x, x+2, color="red", linewidth=0.50)
-ax.plot(x, x+3, color="red", linewidth=1.00)
-ax.plot(x, x+4, color="red", linewidth=2.00)
+ax.plot(x, x + 1, color="red", linewidth=0.25)
+ax.plot(x, x + 2, color="red", linewidth=0.50)
+ax.plot(x, x + 3, color="red", linewidth=1.00)
+ax.plot(x, x + 4, color="red", linewidth=2.00)
 
 # possible linestype options ‘-‘, ‘–’, ‘-.’, ‘:’, ‘steps’
-ax.plot(x, x+5, color="green", lw=3, linestyle='-')
-ax.plot(x, x+6, color="green", lw=3, ls='-.')
-ax.plot(x, x+7, color="green", lw=3, ls=':')
+ax.plot(x, x + 5, color="green", lw=3, linestyle='-')
+ax.plot(x, x + 6, color="green", lw=3, ls='-.')
+ax.plot(x, x + 7, color="green", lw=3, ls=':')
 
 # custom dash
-line, = ax.plot(x, x+8, color="black", lw=1.50)
-line.set_dashes([5, 10, 15, 10]) # format: line length, space length, ...
+line, = ax.plot(x, x + 8, color="black", lw=1.50)
+line.set_dashes([5, 10, 15, 10])  # format: line length, space length, ...
 
 # possible marker symbols: marker = '+', 'o', '*', 's', ',', '.', '1', '2', '3', '4', ...
-ax.plot(x, x+ 9, color="blue", lw=3, ls='-', marker='+')
-ax.plot(x, x+10, color="blue", lw=3, ls='--', marker='o')
-ax.plot(x, x+11, color="blue", lw=3, ls='-', marker='s')
-ax.plot(x, x+12, color="blue", lw=3, ls='--', marker='1')
+ax.plot(x, x + 9, color="blue", lw=3, ls='-', marker='+')
+ax.plot(x, x + 10, color="blue", lw=3, ls='--', marker='o')
+ax.plot(x, x + 11, color="blue", lw=3, ls='-', marker='s')
+ax.plot(x, x + 12, color="blue", lw=3, ls='--', marker='1')
 
 # marker size and color
-ax.plot(x, x+13, color="purple", lw=1, ls='-', marker='o', markersize=2)
-ax.plot(x, x+14, color="purple", lw=1, ls='-', marker='o', markersize=4)
-ax.plot(x, x+15, color="purple", lw=1, ls='-', marker='o', markersize=8, markerfacecolor="red")
-ax.plot(x, x+16, color="purple", lw=1, ls='-', marker='s', markersize=8, 
+ax.plot(x, x + 13, color="purple", lw=1, ls='-', marker='o', markersize=2)
+ax.plot(x, x + 14, color="purple", lw=1, ls='-', marker='o', markersize=4)
+ax.plot(x, x + 15, color="purple", lw=1, ls='-', marker='o', markersize=8, markerfacecolor="red")
+ax.plot(x, x + 16, color="purple", lw=1, ls='-', marker='s', markersize=8,
         markerfacecolor="yellow", markeredgewidth=3, markeredgecolor="green");
-
 
 # ### Control over axis appearance
 
@@ -409,43 +423,44 @@ ax.plot(x, x+16, color="purple", lw=1, ls='-', marker='s', markersize=8,
 
 # ## Plot range
 
-# We can configure the ranges of the axes using the `set_ylim` and `set_xlim` methods in the axis object, or `axis('tight')` for automatically getting "tightly fitted" axes ranges:
+# We can configure the ranges of the axes using the `set_ylim` and `set_xlim` methods in the axis object, or
+# `axis('tight')` for automatically getting "tightly fitted" axes ranges:
 
 # In[58]:
 
 
 fig, axes = plt.subplots(1, 3, figsize=(12, 4))
 
-axes[0].plot(x, x**2, x, x**3)
+axes[0].plot(x, x ** 2, x, x ** 3)
 axes[0].set_title("default axes ranges")
 
-axes[1].plot(x, x**2, x, x**3)
+axes[1].plot(x, x ** 2, x, x ** 3)
 axes[1].axis('tight')
 axes[1].set_title("tight axes")
 
-axes[2].plot(x, x**2, x, x**3)
+axes[2].plot(x, x ** 2, x, x ** 3)
 axes[2].set_ylim([0, 60])
 axes[2].set_xlim([2, 5])
 axes[2].set_title("custom axes range");
 
-
 # # Special Plot Types
 # 
-# There are many specialized plots we can create, such as barplots, histograms, scatter plots, and much more. Most of these type of plots we will actually create using seaborn, a statistical plotting library for Python. But here are a few examples of these type of plots:
+# There are many specialized plots we can create, such as barplots, histograms, scatter plots, and much more.
+# Most of these type of plots we will actually create using seaborn, a statistical plotting library for Python.
+# But here are a few examples of these type of plots:
 
 # In[60]:
 
 
-plt.scatter(x,y)
-
+plt.scatter(x, y)
 
 # In[65]:
 
 
 from random import sample
+
 data = sample(range(1, 1000), 100)
 plt.hist(data)
-
 
 # In[69]:
 
@@ -453,14 +468,14 @@ plt.hist(data)
 data = [np.random.normal(0, std, 100) for std in range(1, 4)]
 
 # rectangular box plot
-plt.boxplot(data,vert=True,patch_artist=True);   
-
+plt.boxplot(data, vert=True, patch_artist=True);
 
 # ## Further reading
 
 # * http://www.matplotlib.org - The project web page for matplotlib.
 # * https://github.com/matplotlib/matplotlib - The source code for matplotlib.
-# * http://matplotlib.org/gallery.html - A large gallery showcaseing various types of plots matplotlib can create. Highly recommended! 
+# * http://matplotlib.org/gallery.html - A large gallery showcaseing various types of plots matplotlib can create.
+# Highly recommended!
 # * http://www.loria.fr/~rougier/teaching/matplotlib - A good matplotlib tutorial.
 # * http://scipy-lectures.github.io/matplotlib/matplotlib.html - Another good matplotlib reference.
-# 
+#
